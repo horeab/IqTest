21255fd6cffa29adfefab21d43759dc04d1add3f
Horea <horea.bucerzan@gmail.com>
2019-06-30 15:00:08 +0300

    backbutton ios

--- src/main/java/libgdx/screens/screens/GameOverScreen.java
+++ src/main/java/libgdx/screens/screens/GameOverScreen.java
@@ -1,10 +1,14 @@
 package libgdx.screens.screens;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
+import libgdx.controls.button.MyButton;
+import libgdx.controls.button.builders.BackButtonBuilder;
 import libgdx.controls.label.MyWrappedLabel;
 import libgdx.controls.label.MyWrappedLabelConfigBuilder;
 import libgdx.game.Game;
@@ -36,6 +40,12 @@ public class GameOverScreen extends AbstractScreen {
         setBackgroundColor(RGBColor.WHITE);
         Table table = new Table();
         table.setFillParent(true);
+        if (Gdx.app.getType() == Application.ApplicationType.iOS) {
+            MyButton backButton = new BackButtonBuilder().createScreenBackButton(this);
+            backButton.setX(MainDimen.horizontal_general_margin.getDimen());
+            backButton.setY(ScreenDimensionsManager.getScreenHeight() - MainDimen.vertical_general_margin.getDimen() - backButton.getHeight());
+            addActor(backButton);
+        }
         table.add(createQuestionImage()).row();
         table.add(createInfoLabel()).center().pad(MainDimen.vertical_general_margin.getDimen()).growX().colspan(4).row();
         addActor(table);
